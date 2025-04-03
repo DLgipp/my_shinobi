e9cd8ea7541d20666f189fefb3e1da87f89a52cf
DLgipp <vaviknai@gmail.com>
2025-04-03 11:45:03 +0300

    DA

--- setup_and_run.sh
+++ setup_and_run.sh
@@ -2,9 +2,9 @@
 
 echo "Creating and setting permissions for the MySQL data directory..."
 mkdir -p $HOME/ShinobiSQL
-sudo chown -R 999:999 $HOME/ShinobiSQL
+chown -R 999:999 $HOME/ShinobiSQL
 mkdir -p $HOME/Shinobi
-sudo chown -R 999:999 $HOME/Shinobi
+chown -R 999:999 $HOME/Shinobi
 
 echo "Building the Docker image for Shinobi CCTV..."
 # docker compose build -f docker-compose-main.yml up
